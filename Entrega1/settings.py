2fef1bafa0dddb8481bd3f72dde07e0fc9569e68
FelipeIgnacio <felipebastidas2013@gmail.com>
2022-08-09 21:31:19 -0400

    Cambios en Base de Datos

--- Entrega1/settings.py
+++ Entrega1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i3&y8lzbka(s2&gkd6pdoin@w+#^gq&ko4oq2pj^+n-e*m_*b%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','fecoder.herokuapp.com','herokuapp.com','127.0.0.1']
@@ -78,21 +78,21 @@ WSGI_APPLICATION = 'Entrega1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-""" import dj_database_url
+import dj_database_url
 from decouple import config
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
     
- } """
+ } 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
