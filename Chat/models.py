689362d467a04f42350c2040b84fa99044ef2366
FelipeIgnacio <felipebastidas2013@gmail.com>
2022-08-09 23:27:23 -0400

    Cambios en Readme y otros archivos.

--- Chat/models.py
+++ Chat/models.py
@@ -35,9 +35,9 @@ class Mensaje(models.Model):
 
         for ms in lista :
             if ms.emisor == emisor:
-                a=[0,ms.texto,ms.fecha_mensaje.strftime('%d-%m-%Y %H:%M:%S', time.localtime())]
+                a=[0,ms.texto,ms.fecha_mensaje.strftime('%d-%m-%Y %H:%M:%S')]
             else:
-                a=[1,ms.texto,ms.fecha_mensaje.strftime('%d-%m-%Y %H:%M:%S', time.localtime())]
+                a=[1,ms.texto,ms.fecha_mensaje.strftime('%d-%m-%Y %H:%M:%S')]
             json.append(a)
         return json
 
